e506efbfb41b4b1285590c251a9cc1e60936fa34
Xiaofan Lin <xiaofanlinus@gmail.com>
2022-06-09 22:39:22 -0700

    finish line drawing

--- src/main.js
+++ src/main.js
@@ -56,12 +56,43 @@ let draw_line3 = (x0, y0, x1, y1, color) => {
         }
     }
 };
+let draw_line4 = (x0, y0, x1, y1) => {
+    let transpose = false;
+    if (Math.abs(x1 - x0) < Math.abs(y1 - y0)) {
+        transpose = true;
+        [x0, y0] = [y0, x0];
+        [x1, y1] = [y1, x1];
+    }
+    if (x1 < x0) {
+        [x0, x1] = [x1, x0];
+        [y0, y1] = [y1, y0];
+    }
+    let derror = Math.abs((y1 - y0) / (x1 - x0));
+    let error = 0;
+    let y = y0;
+    for (let x = x0; x <= x1; x++) {
+        if (transpose) {
+            draw_dot(y, x, 0, 0, 0, 255);
+        }
+        else {
+            draw_dot(x, y, 0, 0, 0, 255);
+        }
+        error += derror;
+        console.log(`${error}`);
+        if (error > 1) {
+            y += (y1 > y0) ? 1 : -1;
+            error -= 1;
+        }
+    }
+};
 //clear_canvas();
 console.profile("draw");
-//ctx.fillStyle = "black";
-/* for (let i = 0; i < 100000; i++) {
-    draw_line3(0, 0, 500, 500, "rbg(0,0,0)");
+ctx.fillStyle = "black";
+/* for (let i = 0; i < 1000000; i++) {
+    //draw_line3(0, 0, 500, 500, "rbg(0,0,0)");
+    draw_line4(0, 0, 500, 500);
 } */
+ctx.putImageData(img_data, 0, 0);
 console.profileEnd("draw");
 console.log(typeof (canvas));
 clear_canvas();
@@ -70,8 +101,10 @@ draw_line2(0, 0, 100, 500, "red");
 draw_line2(0, 0, 500, 100, "blue");
 draw_line2(400, 200, 0, 0, "blue"); */
 clear_canvas();
-draw_line3(0, 0, 500, 500, "black");
-draw_line3(0, 0, 100, 500, "red");
-draw_line3(0, 0, 500, 100, "blue");
-//draw_line3(400, 200, 0, 0, "blue");
+/* draw_line4(0, 0, 500, 500);
+draw_line4(0, 0, 100, 500);
+draw_line4(0, 0, 500, 100);
+draw_line4(400, 200, 0, 0);
+ */
+draw_line4(0, 500, 300, 0);
 ctx.putImageData(img_data, 0, 0);
