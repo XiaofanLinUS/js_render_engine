73f37ae5eb39920403af1cf0366cbd950e819675
Xiaofan Lin <xiaofanlinus@gmail.com>
2022-06-10 21:23:21 -0700

    done with filling triangle #2

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import process from './ObjectParser.js';
+import { Vec2 } from './math/Vec2.js';
 let canvas = document.querySelector("#view");
 let ctx = canvas.getContext('2d');
 let img_data;
@@ -43,6 +44,8 @@ let draw_dot = (x, y, c) => {
     if (x >= canvas.width || x < 0 || y >= canvas.height || y < 0) {
         return;
     }
+    x = Math.floor(x);
+    y = Math.floor(y);
     y = canvas.height - y;
     let idx = (x + y * canvas.width) * 4;
     //ctx.fillStyle = color;
@@ -158,4 +161,77 @@ for (let f in faces) {
         draw_line4(x0, y0, x1, y1, black);
     }
 }
+paint_canvas();
+clear_canvas();
+let draw_line4_v = (p0, p1, color) => {
+    draw_line4(p0.x, p0.y, p1.x, p1.y, color);
+};
+let draw_triangle = (v1, v2, v3, color) => {
+    draw_line4_v(v1, v2, color);
+    draw_line4_v(v2, v3, color);
+    draw_line4_v(v3, v1, color);
+};
+let v1 = new Vec2(20, 50);
+let v2 = new Vec2(400, 100);
+let v3 = new Vec2(100, 300);
+draw_triangle(v1, v2, v3, red);
+let fill_triangle = (v1, v2, v3, color) => {
+    if (v1.y == v2.y && v1.y == v3.y) {
+        return;
+    }
+    let vs = [v1, v2, v3];
+    vs.sort((a, b) => a.y < b.y ? -1 : 1);
+    //vs.map(console.log);
+    for (let y = vs[0].y; y <= vs[2].y; y++) {
+        let x_l, x_r;
+        let x1 = vs[0].x;
+        let x2 = vs[1].x;
+        let x3 = vs[2].x;
+        let y1 = vs[0].y;
+        let y2 = vs[1].y;
+        let y3 = vs[2].y;
+        let delta_y = y - y1;
+        x_r = (x3 - x1) / (y3 - y1) * delta_y + x1;
+        if (y <= y2 && y1 != y2) {
+            // y2 == y3
+            x_l = (x2 - x1) / (y2 - y1) * delta_y + x1;
+        }
+        else {
+            delta_y = y - y2;
+            x_l = (x3 - x2) / (y3 - y2) * delta_y + x2;
+        }
+        //draw_line4(x_l, y, x_r, y, color);
+        if (x_l > x_r) {
+            [x_l, x_r] = [x_r, x_l];
+        }
+        for (let x = x_l; x <= x_r; x++) {
+            draw_dot(x, y, color);
+            draw_dot(x, y, color);
+        }
+    }
+};
+paint_canvas();
+clear_canvas();
+// test case 1:
+/*
+fill_triangle(new Vec2(20, 50),
+new Vec2(400, 100),
+new Vec2(100, 300), black);
+ */
+// test case 2:
+fill_triangle(new Vec2(20, 50), new Vec2(90, 100), new Vec2(120, 500), red);
+// test case 3:
+fill_triangle(new Vec2(50, 60), new Vec2(10, 180), new Vec2(80, 300), blue);
+// test case 4:
+fill_triangle(new Vec2(150, 80), new Vec2(30, 120), new Vec2(80, 400), green);
+/* // test case 5
+fill_triangle(new Vec2(150, 80),
+new Vec2(30, 120),
+new Vec2(0, 400), black);
+ */
+/* fill_triangle(new Vec2(150, 0),
+new Vec2(30, 0),
+new Vec2(0, 400), black);
+ */
+fill_triangle(new Vec2(150, 0), new Vec2(30, 400), new Vec2(0, 400), black);
 paint_canvas();
