5f5416ec3a1c0fd6da4d55abd161f7a8bcfc43bc
Xiaofan Lin <xiaofanlinus@gmail.com>
2022-06-22 22:14:36 -0700

    texture support from shader #13

--- src/main.ts
+++ src/main.ts
@@ -48,8 +48,9 @@ faces = model.faces;
 
 
 let texture_img: Img = await load_img('../res/head.png');
+let normal_img: Img = await load_img('../res/head_nm.png');
 
-let center = new Vec3(10, 10, 10);
+let center = new Vec3(1, 1, 1.5);
 let target  = new Vec3(0, 0, 0);
 let up = new Vec3(0, 1, 0);
 let camera = Mat4.lookat(center, target, up);
@@ -58,56 +59,80 @@ let light_dir = new Vec3(1, 1, 1);
 light_dir.normalize();
 
 let perspective = new Mat4();
-perspective.data[3][2] = - 1 / (center.sub(target).norm());
+perspective.data[3][2] = - 1 / 2;
 
 let screen = Mat4.viewport(0, 0, canvas.width, canvas.height);
 let combined = screen.mul(perspective.mul(camera));
 
 
 
+function sample_img(img: Img, t_coord: Vec2): Color {
+    let t_x = Math.floor(t_coord.x() * img.w) - 1;
+    let t_y = img.h  - (Math.floor(t_coord.y() * img.h) - 1) - 1;
+    let t_idx = 4 * (t_x + t_y * img.w);
+
+
+    let color = {
+        r: img.data[t_idx],
+        g: img.data[t_idx + 1],
+        b: img.data[t_idx + 2],
+        a: 255
+    }
+    return color;
+}
 
 class GouraudShader extends Shader {
-    private intensity: Vec3
-
+    private intensity: number[]
+    private t_coord: Vec2[]
     constructor() {
         super();
-        this.intensity = new Vec3(0, 0, 0);
+        this.intensity = new Array(3).fill(0);
+        this.t_coord = new Array(3);
     }
 
-    override vertex({v, n, idx}: VertexData): Vec3 {
+    override vertex({v, n, t, idx}: VertexData): Vec3 {
         // screen coordinate
         let s = Vec4.from_num_arr(v.data.concat(1));
         // internsity element
         let i = light_dir.dot(n);
 
         s = combined.mul_v4(s).to_p();
-        this.intensity.data[idx] = i;
+
+
+
+        this.intensity[idx] = i;
+        this.t_coord[idx] = t;
 
         return Vec3.from_num_arr(s.data);
     }
 
 
-    override fragment(bary: Vec3) {
-        
-        let final_i = this.intensity.dot(bary);
-        let r = final_i * 255;
-        let g = final_i * 255;
-        let b = final_i * 255;
+    override fragment(bary: Vec3) {        
+        let final_intensity = this.intensity[0] * bary.x() + this.intensity[1] * bary.y() + this.intensity[2] * bary.z();
+        let final_t_coord   = this.t_coord[0].mul(bary.x()).add(this.t_coord[1].mul(bary.y())).add(this.t_coord[2].mul(bary.z()));
 
+        let color = sample_img(texture_img, final_t_coord);
 
+        if(final_intensity>.85) final_intensity = 1;
+        else if (final_intensity>.60) final_intensity = .80;
+        else if (final_intensity>.45) final_intensity = .60;
+        else if (final_intensity>.30) final_intensity = .45;
+        else if (final_intensity>.15) final_intensity = .30;
+        else final_intensity = 0;
+
+        let r = final_intensity * color.r;
+        let g = final_intensity * color.g;
+        let b = final_intensity * color.b;
 
         return {r: r, g: g, b: b, a: 255};
     }
 }
 
 let g_shader = new GouraudShader();
-let z_buff = (new Array(canvas.height * canvas.width)).fill(-1000);
+let z_buff = (new Array(canvas.height * canvas.width)).fill(-Infinity);
 function step() {
-    paper.clear();
-    
-    
+    paper.clear();        
     // z buffer used to perform z-test
-    
     for (let f in faces) {
         
         let face = faces[f];
@@ -117,6 +142,7 @@ function step() {
             let v_data = new VertexData();
             v_data.v = face.get_vertex(idx).data;
             v_data.n = face.get_normal(idx);
+            v_data.t = face.get_t_coord(idx);
             v_data.idx = idx;
             s_coords.push(g_shader.vertex(v_data));
         }
